58c74d6380091ec1c68126cd0485026ed90370d6
Tom Harris <tkmharris@gmail.com>
2017-09-07 21:57:29 +0100

    something funny going on

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,19 @@
 set -ex
 
 # Make sure _site is up-to-date
-bundle exec jekyll build
+jekyll build
 
 # Ensure any commits are pushed to github
-git push 
+git push
 
 # Switch to master, which is where we keep the built pages
 git checkout master
 
-# Clean the directory, so it will only contain generated files, CNAME and git
-# files
-find . -maxdepth 1 -not -name CNAME -a -not -name '.git*' -a -not -name _site -a -not -name . -exec rm -r {} \;
+# Clean the directory so it only contains CNAME and git files
+find . -maxdepth 1 -not -name CNAME -a -not -name '.git*' -a -not -name . -exec rm -r {} \;
+
+# Import _site folder from source
+git checkout source -- _site
 
 # Copy the generated files into the root directory
 cp -r _site/* .
@@ -22,11 +24,10 @@ rm -r _site
 
 # Commit the new content
 git add .
-git commit -am "build: $(date)" 
+git commit -am "build: $(date)"
 
 # Push to github, to reflect the changes on my website
 git push
 
 # By default, we stay in the source branch, for ease of editing
 git checkout source
-
